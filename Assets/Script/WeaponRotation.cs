fe116b6050e2f454689ae657a4dc274e4799c98f
Li Steven <675384631@qq.com>
2019-04-17 21:58:24 -0700

    rotatoins update

--- Assets/Script/WeaponRotation.cs
+++ Assets/Script/WeaponRotation.cs
@@ -15,10 +15,27 @@ public class WeaponRotation : MonoBehaviour
 
     private float rotation = 0;
 
+    bool HasMouseMoved()
+    {
+        return (Input.GetAxis("Mouse X") != 0 || (Input.GetAxis("Mouse Y") != 0));
+    }
+
     // Update is called once per frame
     void Update()
     {
 
-        gameObject.transform.Rotate(Input.mousePosition);
+        if (HasMouseMoved())
+        {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = 0;
+
+            Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+            mousePos.x = mousePos.x - objectPos.x;
+            mousePos.y = mousePos.y - objectPos.y;
+
+            float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+            angle -= 180;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        }
     }
 }

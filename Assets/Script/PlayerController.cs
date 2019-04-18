daf835e817a199b55363099eb8321b57a5b95699
Li Steven <675384631@qq.com>
2019-04-18 09:45:54 -0700

    update

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Vector2 playerpos;
+    void start()
+    {
+        playerpos = transform.position;
+    }
+
+    void update()
+    {
+
+    }
+}
--- Assets/Script/WeaponRotation.cs
+++ Assets/Script/WeaponRotation.cs
@@ -9,28 +9,21 @@ public class WeaponRotation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        GameObject Player = GameObject.Find("Player");
+        PlayerController playerController = Player.GetComponent<PlayerController>();
 
-    }
-
-
-    private float rotation = 0;
-
-    bool HasMouseMoved()
-    {
-        return (Input.GetAxis("Mouse X") != 0 || (Input.GetAxis("Mouse Y") != 0));
     }
 
     public float speed = 5f;
     // Update is called once per frame
     void Update()
     {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotationalAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rotationalAngle -= 180;
+        Quaternion rotation = Quaternion.AngleAxis(rotationalAngle, Vector3.forward);
 
-            Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            float rotationalAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            rotationalAngle -= 180;
-            Quaternion rotation = Quaternion.AngleAxis(rotationalAngle, Vector3.forward);
-
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
 
     }
 }
